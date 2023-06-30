f7302b4189f9d6147ee41a979e694eba823907c0
ViadimirGolubev <golubevvlvl@mail.ru>
2023-06-30 23:42:18 +0300

    Save Program.cs3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿
 //  Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
@@ -16,7 +15,7 @@
 // {
 //     if (num1 == num2)
 //     {
-//         return num1.ToString(); // условие выхода из рекусии
+//         return num1.ToString(); // условие выхода из рекурсии
 //     }
 //     return (num1 + " " + ConclusionNum(num1 - 1, num2));
 // }
@@ -26,47 +25,56 @@
 
 
 
-// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+// // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// // M = 1; N = 15 -> 120
+// // M = 4; N = 8. -> 30
 
-// Console.Write("Введите число = ");
-// int number1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите число = ");
-// int number2 = Convert.ToInt32(Console.ReadLine());
 
-// Console.Write(PrintNum(number1, number2) + 1);// вызов метода внутри
+Console.Write("Введите число M = ");
+int  m= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N = ");
+int  n= Convert.ToInt32(Console.ReadLine());
 
-// string PrintNum(int num1, int num2) // база метода и рекуссии будет как и в первой задаси
+Console.Write(PrintNum(m, n));
+
+int PrintNum(int num1, int num2)
+{
+    int sum = 0;
+    int count = num1;
+    {
+    if (num1 == num2||num1 > num2)
+        return 0;
+    else
+    
+       while (count <= num2)
+    {
+        sum = sum + count;
+        count = count + 1;
+    }
+    return sum;
+    }
+}
+
+// Console.Write("Введите число M = ");
+// int  m= Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N = ");
+// int  n= Convert.ToInt32(Console.ReadLine());
+
+// int x()
 // {
-// if (num1>num2)// но здесь проверку делаем на то что перое число больше втрого
-// {
-// return "Нет решения";// тут без консоль врайт лайн пишем нет решения аргумент
+//     int result = 0;
+//     int count = m;
+//     if (n == m||n > m)
+//          result = 0;
+//     else
+//     while (count <= n)
+//     {
+//         result = result + count;
+//         count = count + 1;
+//     }
+//     return result;
 // }
-// if (num1==num2)
-// {
-// return num1.ToString();// тут остановка рекурсии
-// }
-
-// return (num1+ PrintNum(num1+1, num2));
-// } 
-// Console.Write("Введите число M: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите число N: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-
-// int functionAkkerman = Ack(m, n);
-
-// Console.Write($"Функция Аккермана = {functionAkkerman} ");
-
-// int Ack(int m, int n)
-// {
-//     if (m == 0) return n + 1;
-//     else if (n == 0 && m>0) return Ack(m - 1, 1);
-//     else return Ack(m - 1, Ack(m, n - 1));
-// }
-
-
+// Console.WriteLine(x());
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
